dc31642104b414931b2660c38b4f707251702865
BlueContext <kevin.w.baker@live.com>
2016-04-27 12:02:57 -0500

    Re-adding arduino project

---
+++ WindowsIoT/IoTSensorProject_FieldModules/IoTSensorProject_FieldModules/IoTSensorProject_FieldModules.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		IoTSensorProject_FieldModules.ino
+ Created:	4/27/2016 12:01:15 PM
+ Author:	Kevin
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
